1717f96d7bee27900ede96a68693e2c8e062c69e
Uttam Rao <55692317+iUttamRao@users.noreply.github.com>
2022-08-31 17:53:05 +0530

    Time: 5 ms (15.82%), Space: 54.6 MB (67.11%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,37 +1,39 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length;j++){
-                if(arr[j-1]==0){
-                    int temp=arr[j-1];
-                    arr[j-1]=arr[j];
-                    arr[j]=temp;
-                }
-            }
-        }
-        
-        
-        
-        
-        
-        // int i=0;
-        // int j=0;
-        // while(i<arr.length && j<arr.length){
-        //     if(arr[i]==0 && arr[j]>0){
-        //         int temp=arr[i];
-        //         arr[i]=arr[j];
-        //         arr[j]=temp;
-        //         i++;
-        //         j++;
-        //     }
-        //     else if(arr[i]>0 && arr[j]==0){
-        //         int temp=arr[i];
-        //         arr[i]=arr[j];
-        //         arr[j]=temp;
-        //     }
-        //     else{
-        //         j++;
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=1;j<arr.length;j++){
+        //         if(arr[j-1]==0){
+        //             int temp=arr[j-1];
+        //             arr[j-1]=arr[j];
+        //             arr[j]=temp;
+        //         }
         //     }
         // }
+        
+        
+        
+        
+        
+        int i=0;
+        int j=0;
+        while(i<arr.length && j<arr.length){
+            if(arr[i]==0 && arr[j]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j++;
+            }
+            else if(arr[i]!=0 && arr[j]==0){
+                // int temp=arr[i];
+                // arr[i]=arr[j];
+                // arr[j]=temp;
+                i++;
+            }
+            else{
+                // i++;
+                j++;
+            }
+        }
     }
 }
