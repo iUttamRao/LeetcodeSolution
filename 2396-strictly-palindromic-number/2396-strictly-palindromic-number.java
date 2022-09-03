efcd99cec083796ed90a74302b72a6f758d8f9c5
Uttam Rao <55692317+iUttamRao@users.noreply.github.com>
2022-09-03 22:08:27 +0530

    Time: 0 ms (100.00%), Space: 39 MB (90.91%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    
+//     static boolean isPal(String str){
+//         int low = 0;
+//         int high = str.length() - 1;
+//         while(low < high){
+//             if(str.charAt(low) != str.charAt(high)){
+//                 return false;
+//             }
+//             low++;
+//             high--;
+//         }
+// return true;
+//     }
+    
+    
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+        //\U0001f923\U0001f923\U0001f923
+//        String arr[] = new String[n-3];
+//         int t = n;
+//         for(int i=2; i <= n-2 ; i++){
+//             String str="";
+//             while(n > 0){
+//                 int rem = n % i;
+//                 str += String.valueOf(rem);
+//                 n = n / i;
+//             }
+//             n = t;
+//             arr[i-2] = str;
+//         }
+        
+//         for(int i=0; i<arr.length; i++){
+//             if(!isPal(arr[i])){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+    }
+}
