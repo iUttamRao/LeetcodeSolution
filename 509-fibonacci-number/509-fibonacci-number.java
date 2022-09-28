1f1301edb485ef310264e4d0830bfa33a51a7fa2
Uttam Rao <55692317+iUttamRao@users.noreply.github.com>
2022-09-29 02:06:01 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.91%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
+        if(n==0){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int cache[] = new int[n+1];
+        cache[0]=0;
+        cache[1]=1;
+        for(int i=2;i<=n;i++){
+            cache[i]=cache[i-1]+cache[i-2];
+        }
+        return cache[n];
     }
 }
