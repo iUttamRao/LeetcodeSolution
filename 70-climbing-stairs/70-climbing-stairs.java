0e29cacaef5adb471fcf8e34fc12a03ec0b142fd
Uttam Rao <55692317+iUttamRao@users.noreply.github.com>
2022-09-30 22:31:33 +0530

    Time: 1 ms (10.10%), Space: 40.5 MB (76.20%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int cache[] = new int[n+1];
+        if(n==0 || n==1){
+            return n;
+        }
+        cache[1]=1;
+        cache[2]=2;
+        for(int i=3;i<=n;i++){
+            cache[i]=cache[i-1]+cache[i-2];
+        }
+        return cache[n];
+    }
+}
